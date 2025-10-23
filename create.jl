9ba108fb76d4dc246c8ab895cfd72097c0370bd8
gbaraldi <baraldigabriel@gmail.com>
2025-10-23 13:46:53 -0300

    Update artifacts

--- create.jl
+++ create.jl
@@ -1,25 +1,24 @@
 using ArtifactUtils, Base.BinaryPlatforms
 add_artifact!(
-                     "Artifacts.toml",
-                     "JuliaNoGPL",
-                     "https://github.com/gbaraldi/a_julia_artifact/releases/download/julia-1.12-nogpl/julia-cde8038cc4-linux-x86_64.tar.gz",
-                     force=true,
-                     platform=Platform("x86_64", "linux")
-                    )
-
-using ArtifactUtils, Base.BinaryPlatforms
-add_artifact!(
-                     "Artifacts.toml",
-                     "JuliaNoGPL",
-                     "https://github.com/gbaraldi/a_julia_artifact/releases/download/julia-1.12-nogpl/julia-cde8038cc4-macos-aarch64.tar.gz",
-                     force=true,
-                     platform=Platform("aarch64", "macos")
-                    )
+    "Artifacts.toml",
+    "JuliaNoGPL",
+    "https://github.com/gbaraldi/a_julia_artifact/releases/download/julia-1.12-nogpl-1/julia-cdaa753cd7-linux-x86_64-stripped.tar.gz",
+    force=true,
+    platform=Platform("x86_64", "linux")
+)
 
 add_artifact!(
     "Artifacts.toml",
     "JuliaNoGPL",
-    "https://github.com/gbaraldi/a_julia_artifact/releases/download/julia-1.12-nogpl/julia-cde8038cc4-windows-x86_64.tar.gz",
+    "https://github.com/gbaraldi/a_julia_artifact/releases/download/julia-1.12-nogpl-1/julia-cdaa753cd7-macos-aarch64-stripped.tar.gz",
+    force=true,
+    platform=Platform("aarch64", "macos")
+)
+
+add_artifact!(
+    "Artifacts.toml",
+    "JuliaNoGPL",
+    "https://github.com/gbaraldi/a_julia_artifact/releases/download/julia-1.12-nogpl-1/julia-cdaa753cd7-windows-x86_64-stripped.tar.gz",
     force=true,
     platform=Platform("x86_64", "windows")
 )
